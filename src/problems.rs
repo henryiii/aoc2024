d66ea537e375d422215ffdd0ee12d835399f9975
Henry Schreiner <henryschreineriii@gmail.com>
2024-12-04 20:16:08 -0500

    feat: timer for solutions too
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- src/problems.rs
+++ src/problems.rs
@@ -23,11 +23,15 @@ pub fn run<T: Problem>(name: &str) {
     let now = Instant::now();
     let input = read_input(&format!("data/{name}.txt"));
 
+    let sol_a_time = Instant::now();
     let solution_a = T::solution_a(&input);
-    println!("Solution A: {color_blue}{solution_a}{color_reset}");
+    let sol_a_time = sol_a_time.elapsed().as_secs_f64() * 1000.0;
+    println!("Solution A: {color_blue}{solution_a}{color_reset}    ({color_green}{sol_a_time:.3}ms{color_reset})");
 
+    let sol_b_time = Instant::now();
     let solution_b = T::solution_b(&input);
-    println!("Solution B: {color_blue}{solution_b}{color_reset}");
+    let sol_b_time = sol_b_time.elapsed().as_secs_f64() * 1000.0;
+    println!("Solution B: {color_blue}{solution_b}{color_reset}    ({color_green}{sol_b_time:.3}ms{color_reset})");
 
     let time_taken = now.elapsed().as_secs_f64() * 1000.0;
 
