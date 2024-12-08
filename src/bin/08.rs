43266367d61e4532dd37134d4407cd22a2470a91
Henry Schreiner <henryschreineriii@gmail.com>
2024-12-08 01:01:01 -0500

    refactor: nicer day 8
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -1,10 +1,10 @@
 /*!
-# 2024 Day 8: Sample
-##  Simple template
+# 2024 Day 8: Resonant Collinearity
+## Map frequencies
 
 <https://adventofcode.com/2024/day/8>
 
-This is a small example to get started, also functions as a template for new days.
+Frequencies on a map.
 */
 
 use std::collections::HashSet;
@@ -19,42 +19,32 @@ struct Day08 {}
 
 fn solution(map: &Grid<char>, range: Range<i64>) -> i64 {
     let mut antinodes: Grid<bool> = Grid::new(map.rows(), map.cols());
+
+    // Every char that is a node
     let chars: HashSet<char> = map.iter().filter(|&v| *v != '.').copied().collect();
     for c in chars {
+        // Every node of a char
         let locs: Vec<(i64, i64)> = map
             .indexed_iter()
             .filter(|(_, &ch)| ch == c)
             .map(|((x, y), _)| (x.try_into().unwrap(), y.try_into().unwrap()))
             .collect();
-        locs.iter()
-            .cartesian_product(locs.iter())
-            .for_each(|(&a, &b)| {
-                if a != b {
-                    let dir = (b.0 - a.0, b.1 - a.1);
-                    for n in range.clone() {
-                        let pos1 = (a.0 - dir.0 * n, a.1 - dir.1 * n);
-                        let pos2 = (b.0 + dir.0 * n, b.1 + dir.1 * n);
-                        if map.get(pos1.0, pos1.1).is_some() {
-                            antinodes[(pos1.0.try_into().unwrap(), pos1.1.try_into().unwrap())] =
-                                true;
-                        }
-                        if map.get(pos2.0, pos2.1).is_some() {
-                            antinodes[(pos2.0.try_into().unwrap(), pos2.1.try_into().unwrap())] =
-                                true;
-                        }
-                    }
+
+        // Every pair of nodes
+        locs.iter().combinations(2).for_each(|v| {
+            let (&a, &b) = v.iter().collect_tuple().unwrap();
+            let dir = (b.0 - a.0, b.1 - a.1);
+            for n in range.clone() {
+                let pos1 = (a.0 - dir.0 * n, a.1 - dir.1 * n);
+                let pos2 = (b.0 + dir.0 * n, b.1 + dir.1 * n);
+                if map.get(pos1.0, pos1.1).is_some() {
+                    antinodes[(pos1.0.try_into().unwrap(), pos1.1.try_into().unwrap())] = true;
                 }
-            });
-    }
-    #[cfg(test)]
-    {
-        let mut newmap = map.clone();
-        newmap.iter_mut().zip(antinodes.iter()).for_each(|(v, &a)| {
-            if a {
-                *v = '#';
+                if map.get(pos2.0, pos2.1).is_some() {
+                    antinodes[(pos2.0.try_into().unwrap(), pos2.1.try_into().unwrap())] = true;
+                }
             }
         });
-        dbg!(newmap);
     }
     antinodes.iter().filter(|&v| *v).count().try_into().unwrap()
 }
