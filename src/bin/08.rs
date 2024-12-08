3a650b0bd42e73626b4354bdfa91760e31dd3d06
Henry Schreiner <henryschreineriii@gmail.com>
2024-12-08 00:49:57 -0500

    refactor: cleaner day 8
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -8,6 +8,7 @@ This is a small example to get started, also functions as a template for new day
 */
 
 use std::collections::HashSet;
+use std::ops::Range;
 
 use aoc2024::grid::read_char;
 use aoc2024::{run, Problem};
@@ -16,88 +17,57 @@ use itertools::Itertools;
 
 struct Day08 {}
 
+fn solution(map: &Grid<char>, range: Range<i64>) -> i64 {
+    let mut antinodes: Grid<bool> = Grid::new(map.rows(), map.cols());
+    let chars: HashSet<char> = map.iter().filter(|&v| *v != '.').copied().collect();
+    for c in chars {
+        let locs: Vec<(i64, i64)> = map
+            .indexed_iter()
+            .filter(|(_, &ch)| ch == c)
+            .map(|((x, y), _)| (x.try_into().unwrap(), y.try_into().unwrap()))
+            .collect();
+        locs.iter()
+            .cartesian_product(locs.iter())
+            .for_each(|(&a, &b)| {
+                if a != b {
+                    let dir = (b.0 - a.0, b.1 - a.1);
+                    for n in range.clone() {
+                        let pos1 = (a.0 - dir.0 * n, a.1 - dir.1 * n);
+                        let pos2 = (b.0 + dir.0 * n, b.1 + dir.1 * n);
+                        if map.get(pos1.0, pos1.1).is_some() {
+                            antinodes[(pos1.0.try_into().unwrap(), pos1.1.try_into().unwrap())] =
+                                true;
+                        }
+                        if map.get(pos2.0, pos2.1).is_some() {
+                            antinodes[(pos2.0.try_into().unwrap(), pos2.1.try_into().unwrap())] =
+                                true;
+                        }
+                    }
+                }
+            });
+    }
+    #[cfg(test)]
+    {
+        let mut newmap = map.clone();
+        newmap.iter_mut().zip(antinodes.iter()).for_each(|(v, &a)| {
+            if a {
+                *v = '#';
+            }
+        });
+        dbg!(newmap);
+    }
+    antinodes.iter().filter(|&v| *v).count().try_into().unwrap()
+}
+
 impl Problem for Day08 {
     fn solution_a(input: &str) -> i64 {
         let map = read_char(input);
-        let mut antinodes: Grid<bool> = Grid::new(map.rows(), map.cols());
-        let chars: HashSet<char> =
-            HashSet::from_iter(map.iter().filter(|&v| *v != '.').map(|&v| v));
-        for c in chars {
-            let locs: Vec<_> = map
-                .indexed_iter()
-                .filter(|(_, &ch)| ch == c)
-                .map(|((x, y), _)| (x as i64, y as i64))
-                .collect();
-            locs.iter()
-                .cartesian_product(locs.iter())
-                .for_each(|(&a, &b)| {
-                    if a != b {
-                        let dir = (b.0 - a.0, b.1 - a.1);
-                        let pos1 = (a.0 - dir.0, a.1 - dir.1);
-                        let pos2 = (b.0 + dir.0, b.1 + dir.1);
-                        if map.get(pos1.0, pos1.1).is_some() {
-                            antinodes[(pos1.0 as usize, pos1.1 as usize)] = true;
-                        }
-                        if map.get(pos2.0, pos2.1).is_some() {
-                            antinodes[(pos2.0 as usize, pos2.1 as usize)] = true;
-                        }
-                    }
-                });
-        }
-        #[cfg(test)]
-        {
-            let mut newmap = map.clone();
-            newmap.iter_mut().zip(antinodes.iter()).for_each(|(v, &a)| {
-                if a {
-                    *v = '#';
-                }
-            });
-            dbg!(newmap);
-        }
-        antinodes.iter().filter(|&v| *v).count().try_into().unwrap()
+        solution(&map, 1..2)
     }
 
     fn solution_b(input: &str) -> i64 {
         let map = read_char(input);
-        let mut antinodes: Grid<bool> = Grid::new(map.rows(), map.cols());
-        let chars: HashSet<char> =
-            HashSet::from_iter(map.iter().filter(|&v| *v != '.').map(|&v| v));
-        for c in chars {
-            let locs: Vec<_> = map
-                .indexed_iter()
-                .filter(|(_, &ch)| ch == c)
-                .map(|((x, y), _)| (x as i64, y as i64))
-                .collect();
-            locs.iter()
-                .cartesian_product(locs.iter())
-                .for_each(|(&a, &b)| {
-                    if a != b {
-                        let dir = (b.0 - a.0, b.1 - a.1);
-                        let max_n  = map.cols().max(map.rows()) as i64;
-                        for n in 0..max_n {
-                            let pos1 = (a.0 - dir.0*n, a.1 - dir.1*n);
-                            let pos2 = (b.0 + dir.0*n, b.1 + dir.1*n);
-                            if map.get(pos1.0, pos1.1).is_some() {
-                                antinodes[(pos1.0 as usize, pos1.1 as usize)] = true;
-                            }
-                            if map.get(pos2.0, pos2.1).is_some() {
-                                antinodes[(pos2.0 as usize, pos2.1 as usize)] = true;
-                            }
-                        }
-                    }
-                });
-        }
-        #[cfg(test)]
-        {
-            let mut newmap = map.clone();
-            newmap.iter_mut().zip(antinodes.iter()).for_each(|(v, &a)| {
-                if a {
-                    *v = '#';
-                }
-            });
-            dbg!(newmap);
-        }
-        antinodes.iter().filter(|&v| *v).count().try_into().unwrap()
+        solution(&map, 0..(map.cols().max(map.rows()).try_into().unwrap()))
     }
 }
 
