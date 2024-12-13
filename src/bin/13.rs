2cd6d87797c1b5e607f4c6a89936b770df55f3f4
Henry Schreiner <henryschreineriii@gmail.com>
2024-12-13 10:12:28 -0500

    feat: day 13b

--- src/bin/13.rs
+++ src/bin/13.rs
@@ -1,33 +1,46 @@
 /*!
-# 2024 Day 13: Sample
-## Simple template
+# 2024 Day 13: Claw Contraption
+## Reaching a final destination
 
 <https://adventofcode.com/2024/day/13>
 
-This is a small example to get started, also functions as a template for new days.
+This is one where a bit of algebra can greatly simplify the problem. After working out the equation
+with the determinant, Copilot just wrote it for me. :shrug: (It didn't get the 0 case right, though.)
 */
+
+use std::ops::RangeInclusive;
 
 use derive_new::new;
 use regex::Regex;
 
 #[derive(Debug, new)]
 struct Machine {
-    a: (usize, usize),
-    b: (usize, usize),
-    prize: (usize, usize),
+    a: (i64, i64),
+    b: (i64, i64),
+    prize: (i64, i64),
 }
 
 impl Machine {
-    fn valid(&self, a: usize, b: usize) -> bool {
+    fn valid(&self, a: i64, b: i64) -> bool {
         let end_point = (a * self.a.0 + b * self.b.0, a * self.a.1 + b * self.b.1);
         end_point == self.prize
     }
 
-    fn chk_all(&self) -> Option<(usize, usize)> {
-        (0..=100)
-            .flat_map(|a| (0..=100).map(move |b| (a, b)))
+    fn chk_all(&self, rng: RangeInclusive<i64>) -> Option<(i64, i64)> {
+        rng.map(|a| (a, (self.prize.1 - (self.a.1 * a)) / self.b.1))
             .filter(|(a, b)| self.valid(*a, *b))
             .min_by_key(|(a, b)| a * 3 + b)
+    }
+    fn chk_analytical(&self) -> Option<(i64, i64)> {
+        let det = self.a.0 * self.b.1 - self.a.1 * self.b.0;
+        if det == 0 {
+            let min = self.prize.1 / self.b.1;
+            let max = self.prize.0 / self.a.0;
+            return self.chk_all(min..=max);
+        }
+        let a = (self.prize.0 * self.b.1 - self.prize.1 * self.b.0) / det;
+        let b = (self.a.0 * self.prize.1 - self.a.1 * self.prize.0) / det;
+        self.valid(a, b).then_some((a, b))
     }
 }
 
@@ -49,16 +62,20 @@ Prize: X=([[:digit:]]+), Y=([[:digit:]]+)",
     .collect()
 }
 
-fn solution_a(input: &str) -> usize {
-    let machines = read_input(input);
-    machines
+fn solution_a(input: &str) -> i64 {
+    read_input(input)
         .iter()
-        .filter_map(|m| m.chk_all().map(|(a, b)| a * 3 + b))
+        .filter_map(|m| m.chk_all(0..=100).map(|(a, b)| a * 3 + b))
         .sum()
 }
 
-fn solution_b(input: &str) -> usize {
-    0
+fn solution_b(input: &str) -> i64 {
+    let add = 10_000_000_000_000;
+    read_input(input)
+        .iter()
+        .map(|m| Machine::new(m.a, m.b, (m.prize.0 + add, m.prize.1 + add)))
+        .filter_map(|m| m.chk_analytical().map(|(a, b)| a * 3 + b))
+        .sum()
 }
 
 fn main() {
@@ -76,6 +93,6 @@ mod tests {
 
     #[test]
     fn test_sample_b() {
-        assert_eq!(super::solution_b(INPUT), 1);
+        assert_eq!(super::solution_b(INPUT), 875_318_608_908);
     }
 }
