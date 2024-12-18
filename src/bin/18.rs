5f7924d8e433bd6458c0ad06c9c5ccf6c6a6b597
Henry Schreiner <henryschreineriii@gmail.com>
2024-12-18 01:28:06 -0500

    feat: day 18
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

---
+++ src/bin/18.rs
@@ -0,0 +1,98 @@
+/*!
+# 2024 Day 18: Sample
+## Simple template
+
+<https://adventofcode.com/2024/day/18>
+
+This is a small example to get started, also functions as a template for new days.
+*/
+
+use grid::Grid;
+use petgraph::{algo::dijkstra, prelude::*};
+
+type Int = usize;
+
+fn read_input(input: &str) -> Vec<(usize, usize)> {
+    use aoc_parse::{parser, prelude::*};
+
+    parser!(
+        lines(
+            usize "," usize
+        )
+    )
+    .parse(input)
+    .unwrap()
+}
+
+fn make_grid(coords: &[(usize, usize)], size: usize) -> Grid<bool> {
+    let mut map = Grid::new(size, size);
+    for (x, y) in coords {
+        map[(*y, *x)] = true;
+    }
+    map
+}
+
+fn make_graph(map: &Grid<bool>) -> UnGraphMap<(usize, usize), ()> {
+    let mut graph = GraphMap::new();
+    for ((y, x), valid) in map.indexed_iter() {
+        if !*valid {
+            graph.add_node((x, y));
+        }
+    }
+    for ((y, x), valid) in map.indexed_iter() {
+        if !*valid {
+            for (dy, dx) in &[(0, 1), (1, 0)] {
+                let (ny, nx) = (y + dy, x + dx);
+                if map.get(ny, nx) == Some(&false) {
+                    graph.add_edge((x, y), (nx, ny), ());
+                }
+            }
+        }
+    }
+    graph
+}
+
+fn solution_a(input: &str) -> Int {
+    let coords = read_input(input);
+    let size = if coords.len() < 1000 { 7 } else { 71 };
+    let nbytes = if coords.len() < 1000 { 12 } else { 1024 };
+    let map = make_grid(&coords[0..nbytes], size);
+    let graph = make_graph(&map);
+    let costs = dijkstra(&graph, (0, 0), Some((size - 1, size - 1)), |_| 1);
+    *costs.get(&(size - 1, size - 1)).unwrap()
+}
+
+fn solution_b(input: &str) -> String {
+    let coords = read_input(input);
+    let size = if coords.len() < 1000 { 7 } else { 71 };
+    let nbytes = if coords.len() < 1000 { 12 } else { 1024 };
+    let map = make_grid(&coords[0..nbytes], size);
+    let mut graph = make_graph(&map);
+    for (x, y) in &coords[nbytes..] {
+        graph.remove_node((*x, *y));
+        let costs = dijkstra(&graph, (0, 0), Some((size - 1, size - 1)), |_| 1);
+        if !costs.contains_key(&(size - 1, size - 1)) {
+            return format!("{x},{y}");
+        }
+    }
+    unreachable!();
+}
+
+fn main() {
+    aoc2024::run("18", solution_a, solution_b);
+}
+
+#[cfg(test)]
+mod tests {
+    const INPUT: &str = include_str!("../../samples/18.txt");
+
+    #[test]
+    fn test_sample_a() {
+        assert_eq!(super::solution_a(INPUT), 22);
+    }
+
+    #[test]
+    fn test_sample_b() {
+        assert_eq!(super::solution_b(INPUT), "6,1".to_string());
+    }
+}
