e7792d0eb90e07da9fd660a30041b7942913c14d
Henry Schreiner <henryschreineriii@gmail.com>
2024-12-20 16:19:09 -0500

    tests: nicer names
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- crates/aoc/src/macros.rs
+++ crates/aoc/src/macros.rs
@@ -3,7 +3,7 @@ macro_rules! make_test {
     ($part:literal, $input:literal, $answer:literal) => {
         paste::paste! {
             #[test]
-            fn [<test_part $part>]() {
+            fn [<test_part_ $part>]() {
                 assert_eq!(super::[<solution_ $part>](include_str!(concat!("../../../samples/", $input))), $answer);
             }
         }
