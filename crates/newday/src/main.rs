2e4cbbda79bab8dca534e9f2ccb594d2e631097d
Henry Schreiner <henryschreineriii@gmail.com>
2024-12-19 23:21:06 -0500

    feat: add newday file modification
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- crates/newday/src/main.rs
+++ crates/newday/src/main.rs
@@ -9,6 +9,7 @@ use std::io::Write;
 use clap::Parser;
 use derive_new::new;
 use serde::Serialize;
+use thiserror::Error;
 use tinytemplate::TinyTemplate;
 
 use getdata::get_input;
@@ -34,7 +35,23 @@ struct Context {
     day: String,
 }
 
+#[derive(Debug, Error)]
+#[error("Failed to find 01..=??")]
+pub struct NoReplaceError {}
+
 const TMPL: &str = include_str!("template.rs.in");
+
+fn replace_end_day(s: &mut [u8], day: u8) -> Result<(), NoReplaceError> {
+    let snippet = b"seq!(N in 01..=";
+    let loc = s
+        .windows(snippet.len())
+        .position(|w| w == snippet)
+        .ok_or(NoReplaceError {})?;
+    let loc = loc + snippet.len();
+    s[loc] = format!("{:02}", day).as_bytes()[0];
+    s[loc + 1] = format!("{:02}", day).as_bytes()[1];
+    Ok(())
+}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let opts: Opts = Opts::parse();
@@ -54,7 +71,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let data = Context::new(opts.year, day.clone());
     let rendered = tt.render("template", &data)?;
     // Using create_new to avoid overwriting existing files, instead of simpler std::fs::write
-    File::create_new(format!("crates/year_{year}/src/{day}.rs"))?.write_all(rendered.as_bytes())?;
+    File::create_new(format!("crates/year_{year}/src/day_{day}.rs"))?
+        .write_all(rendered.as_bytes())?;
     File::create_new(format!("samples/{year}/{day}.txt"))?.write_all(b"")?;
 
     let data = if let Some(session) = opts.session {
@@ -63,5 +81,13 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         String::new()
     };
     File::create_new(format!("data/{year}/{day}.txt"))?.write_all(data.as_bytes())?;
+
+    let mut lib_txt = std::fs::read(format!("crates/year_{year}/src/lib.rs"))?;
+    replace_end_day(&mut lib_txt, opts.day)?;
+    std::fs::write(format!("crates/year_{year}/src/lib.rs"), &lib_txt)?;
+
+    let mut main_txt = std::fs::read(format!("crates/year_{year}/src/main.rs"))?;
+    replace_end_day(&mut main_txt, opts.day)?;
+    std::fs::write(format!("crates/year_{year}/src/main.rs"), &main_txt)?;
     Ok(())
 }
