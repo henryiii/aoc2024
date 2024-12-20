b616633d2c423a30cb03687f2b42f418a8f90301
Henry Schreiner <henryschreineriii@gmail.com>
2024-12-20 14:41:10 -0500

    docs: replace sample text
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- crates/year_2024/src/day_17.rs
+++ crates/year_2024/src/day_17.rs
@@ -1,10 +1,10 @@
 /*!
-# 2024 Day 17: Sample
-## Simple template
+# 2024 Day 17: Chronospatial Computer
+## Instructions and registers
 
 <https://adventofcode.com/2024/day/17>
 
-This is a small example to get started, also functions as a template for new days.
+My part 2 just happens to work for my input, but it's not general enough.
 */
 
 use std::ops::BitXor;
