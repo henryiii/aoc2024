7999e1c0a954d4f1ca647dbd225424e40f192cb1
Henry Schreiner <henryschreineriii@gmail.com>
2024-12-24 11:29:39 -0500

    refactor: implement struct for day 21
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- crates/year_2024/src/day_21.rs
+++ crates/year_2024/src/day_21.rs
@@ -12,72 +12,124 @@ This looks great: <https://www.reddit.com/r/adventofcode/comments/1hkc7h7/2024_d
 type Int = usize;
 
 use std::iter;
-use std::sync::LazyLock;
 
 use counter::Counter;
 use grid::{grid, Grid};
 use itertools::Itertools;
 
-// 319556127176302 is too high
-// 312741267418362 is too high
-// 193606477851200 is too high
-
-static NUMERIC_KEYPAD: LazyLock<Grid<char>> = LazyLock::new(|| {
-    grid![
-        ['7', '8', '9']
-        ['4', '5', '6']
-        ['1', '2', '3']
-        ['x', '0', 'A']
-    ]
-});
-
-static DIR_KEYPAD: LazyLock<Grid<char>> = LazyLock::new(|| {
-    grid![
-        ['x', '^', 'A']
-        ['<', 'v', '>']
-    ]
-});
-
-fn pos(current: char, pad: &Grid<char>) -> (usize, usize) {
-    pad.indexed_iter().find(|(_, &c)| c == current).unwrap().0
+struct KeyPad {
+    pad: Grid<char>,
 }
 
-fn valid_path(current: char, path: &[char], pad: &Grid<char>) -> Option<char> {
-    path.iter()
-        .try_fold(current, |acc, key| step(acc, *key, pad))
-}
+impl KeyPad {
+    fn new_numeric() -> Self {
+        Self {
+            pad: grid![
+                ['7', '8', '9']
+                ['4', '5', '6']
+                ['1', '2', '3']
+                ['x', '0', 'A']
+            ],
+        }
+    }
 
-fn paths(current: char, target: char, pad: &Grid<char>) -> Vec<Vec<char>> {
-    let cur_pos = pad.indexed_iter().find(|(_, &c)| c == current).unwrap().0;
-    let new_pos = pad.indexed_iter().find(|(_, &c)| c == target).unwrap().0;
+    fn new_dir() -> Self {
+        Self {
+            pad: grid![
+                ['x', '^', 'A']
+                ['<', 'v', '>']
+            ],
+        }
+    }
 
-    let vert_chars = if cur_pos.0 < new_pos.0 {
-        iter::repeat('v').take(new_pos.0 - cur_pos.0)
-    } else {
-        iter::repeat('^').take(cur_pos.0 - new_pos.0)
-    };
-    let horiz_chars = if cur_pos.1 < new_pos.1 {
-        iter::repeat('>').take(new_pos.1 - cur_pos.1)
-    } else {
-        iter::repeat('<').take(cur_pos.1 - new_pos.1)
-    };
+    fn indexed_iter(&self) -> impl Iterator<Item = ((usize, usize), &char)> {
+        self.pad.indexed_iter().filter(|(_, &c)| c != 'x')
+    }
 
-    let vals1: Vec<_> = vert_chars
-        .clone()
-        .chain(horiz_chars.clone())
-        .chain(iter::once('A'))
-        .collect();
-    let vals2: Vec<_> = horiz_chars
-        .chain(vert_chars)
-        .chain(iter::once('A'))
-        .collect();
+    fn iter(&self) -> impl Iterator<Item = &char> {
+        self.pad.iter().filter(|&c| *c != 'x')
+    }
 
-    [vals1, vals2]
-        .iter()
-        .unique()
-        .filter(|v| valid_path(current, v, pad).is_some())
-        .cloned()
-        .collect()
+    fn pos(&self, current: char) -> (usize, usize) {
+        self.indexed_iter().find(|(_, &c)| c == current).unwrap().0
+    }
+
+    fn valid_path(&self, current: char, path: &[char]) -> Option<char> {
+        path.iter()
+            .try_fold(current, |acc, key| self.step(acc, *key))
+    }
+
+    fn paths(&self, current: char, target: char) -> Vec<Vec<char>> {
+        let cur_pos = self.pos(current);
+        let new_pos = self.pos(target);
+
+        let vert_chars = if cur_pos.0 < new_pos.0 {
+            iter::repeat('v').take(new_pos.0 - cur_pos.0)
+        } else {
+            iter::repeat('^').take(cur_pos.0 - new_pos.0)
+        };
+        let horiz_chars = if cur_pos.1 < new_pos.1 {
+            iter::repeat('>').take(new_pos.1 - cur_pos.1)
+        } else {
+            iter::repeat('<').take(cur_pos.1 - new_pos.1)
+        };
+
+        let vals1: Vec<_> = vert_chars
+            .clone()
+            .chain(horiz_chars.clone())
+            .chain(iter::once('A'))
+            .collect();
+        let vals2: Vec<_> = horiz_chars
+            .chain(vert_chars)
+            .chain(iter::once('A'))
+            .collect();
+
+        [vals1, vals2]
+            .iter()
+            .unique()
+            .filter(|v| self.valid_path(current, v).is_some())
+            .cloned()
+            .collect()
+    }
+
+    fn step(&self, current: char, key: char) -> Option<char> {
+        let (x, y) = self.pos(current);
+        let new_key = match key {
+            '^' => *self.pad.get(x - 1, y)?,
+            'v' => *self.pad.get(x + 1, y)?,
+            '<' => *self.pad.get(x, y - 1)?,
+            '>' => *self.pad.get(x, y + 1)?,
+            'A' => current,
+            _ => unreachable!(),
+        };
+        if new_key == 'x' {
+            return None;
+        }
+        Some(new_key)
+    }
+
+    fn calc_next(&self, prev: &Counter<(char, char)>) -> Counter<(char, char)> {
+        // Compute every possible pair
+        self.indexed_iter()
+            .flat_map(|((_, _), ki)| {
+                self.indexed_iter().map(|((_, _), kf)| {
+                    let paths = self.paths(*ki, *kf);
+                    let key_presses = paths
+                        .into_iter()
+                        .map(|path| {
+                            iter::once('A')
+                                .chain(path)
+                                .tuple_windows()
+                                .map(|(i, f)| prev[&(i, f)])
+                                .sum()
+                        })
+                        .min()
+                        .unwrap();
+                    ((*ki, *kf), key_presses)
+                })
+            })
+            .collect()
+    }
 }
 
 fn read_input(input: &str) -> Vec<Vec<char>> {
@@ -88,99 +140,16 @@ fn read_input(input: &str) -> Vec<Vec<char>> {
         .unwrap()
 }
 
-fn step(current: char, key: char, pad: &Grid<char>) -> Option<char> {
-    let (x, y) = pos(current, pad);
-    let new_key = match key {
-        '^' => *pad.get(x - 1, y)?,
-        'v' => *pad.get(x + 1, y)?,
-        '<' => *pad.get(x, y - 1)?,
-        '>' => *pad.get(x, y + 1)?,
-        'A' => current,
-        _ => unreachable!(),
-    };
-    if new_key == 'x' {
-        return None;
-    }
-    Some(new_key)
-}
+fn solution(input: &str, robots: usize) -> Int {
+    let numeric_keypad = KeyPad::new_numeric();
+    let dir_keypad = KeyPad::new_dir();
 
-fn calc_next(prev: &Counter<(char, char)>, pad: &Grid<char>) -> Counter<(char, char)> {
-    // Compute every possible pair
-    pad.indexed_iter()
-        .filter(|(_, c)| **c != 'x')
-        .flat_map(|((_, _), ki)| {
-            pad.indexed_iter()
-                .filter(|(_, c)| **c != 'x')
-                .map(|((_, _), kf)| {
-                    let paths = paths(*ki, *kf, pad);
-                    let key_presses = paths
-                        .into_iter()
-                        .map(|path| {
-                            iter::once('A')
-                            .chain(path.into_iter())
-                                .tuple_windows()
-                                .map(|(i, f)| prev[&(i, f)])
-                                .sum()
-                        })
-                        .min()
-                        .unwrap();
-                    ((*ki, *kf), key_presses)
-                })
-        })
-        .collect()
-}
-
-fn expand(path: &[char], pad: &Grid<char>) -> Vec<Vec<char>> {
-    // Add the starting position
-    iter::once(&'A')
-        .chain(path.iter())
-        .tuple_windows()
-        .map(|(current, target)| paths(*current, *target, pad))
-        .reduce(|a, b| {
-            a.iter()
-                .cartesian_product(b.iter())
-                .map(|(x, y)| x.iter().chain(y).copied().collect())
-                .collect()
-        })
-        .unwrap()
-}
-
-fn solution_a(input: &str) -> Int {
-    let lines = read_input(input);
-    lines
-        .into_iter()
-        .map(|line| {
-            let val: Int = String::from_iter(&line)
-                .strip_suffix("A")
-                .unwrap()
-                .parse()
-                .unwrap();
-
-            let newlines = expand(&line, &NUMERIC_KEYPAD);
-            let newlines = newlines.into_iter().flat_map(|v| expand(&v, &DIR_KEYPAD));
-            let newlines = newlines.into_iter().flat_map(|v| expand(&v, &DIR_KEYPAD));
-            let min = newlines.min_by_key(Vec::len).unwrap().len();
-            val * min
-        })
-        .sum()
-}
-
-fn solution_b(input: &str, robots: usize) -> Int {
-    let my_presses = DIR_KEYPAD
+    let my_presses = dir_keypad
         .iter()
-        .filter(|&k| *k != 'x')
-        .flat_map(|&ki| {
-            DIR_KEYPAD
-                .iter()
-                .filter(|&k| *k != 'x')
-                .map(move |&kf| ((ki, kf), 1))
-        })
+        .flat_map(|&ki| dir_keypad.iter().map(move |&kf| ((ki, kf), 1)))
         .collect();
-    let rob_presses = (0..robots).fold(my_presses, |prev, _| {
-        let ret = calc_next(&prev, &DIR_KEYPAD);
-        ret
-    });
-    let final_presses = calc_next(&rob_presses, &NUMERIC_KEYPAD);
+    let rob_presses = (0..robots).fold(my_presses, |prev, _| dir_keypad.calc_next(&prev));
+    let final_presses = numeric_keypad.calc_next(&rob_presses);
 
     let lines = read_input(input);
     lines
@@ -191,6 +160,7 @@ fn solution_b(input: &str, robots: usize) -> Int {
                 .unwrap()
                 .parse()
                 .unwrap();
+
             iter::once('A')
                 .chain(line)
                 .tuple_windows()
@@ -202,75 +172,30 @@ fn solution_b(input: &str, robots: usize) -> Int {
 }
 
 pub fn main(_: bool) {
-    aoc::run("21", solution_a, |input| solution_b(input, 25));
+    aoc::run(
+        "21",
+        |input| solution(input, 2),
+        |input| solution(input, 25),
+    );
 }
 
 #[cfg(test)]
 mod tests {
-    use std::vec;
-
-    use super::*;
-
-    aoc::make_test!("a", "2024/21.txt", 126_384);
-
+    #[test]
+    fn test_part_1() {
+        assert_eq!(
+            super::solution(include_str!(concat!("../../../samples/", "2024/21.txt")), 2),
+            126_384
+        );
+    }
     #[test]
     fn test_part_b() {
         assert_eq!(
-            super::solution_b(
+            super::solution(
                 include_str!(concat!("../../../samples/", "2024/21.txt")),
                 25
             ),
             154_115_708_116_294
         );
-    }
-
-    #[test]
-    fn test_part_b_short() {
-        assert_eq!(
-            super::solution_b(include_str!(concat!("../../../samples/", "2024/21.txt")), 2),
-            126_384
-        );
-    }
-
-    #[test]
-    fn test_paths_numeric() {
-        assert_eq!(paths('A', '3', &NUMERIC_KEYPAD), vec![vec!['^', 'A']]);
-        assert_eq!(paths('3', '3', &NUMERIC_KEYPAD), vec![vec!['A']]);
-        assert_eq!(paths('3', '1', &NUMERIC_KEYPAD), vec![vec!['<', '<', 'A']]);
-        assert_eq!(paths('0', '1', &NUMERIC_KEYPAD), vec![vec!['^', '<', 'A']]);
-        assert_eq!(
-            paths('3', '7', &NUMERIC_KEYPAD),
-            vec![vec!['^', '^', '<', '<', 'A'], vec!['<', '<', '^', '^', 'A']]
-        );
-    }
-
-    #[test]
-    fn test_paths_dir() {
-        assert_eq!(paths('A', '>', &DIR_KEYPAD), vec![vec!['v', 'A']]);
-        assert_eq!(paths('A', '<', &DIR_KEYPAD), vec![vec!['v', '<', '<', 'A']]);
-    }
-
-    #[test]
-    fn counter_repeated_elements() {
-        let counter: Counter<char> = [('A', 1), ('A', 1), ('A', 2)].into_iter().collect();
-        assert_eq!(counter[&'A'], 4);
-    }
-
-    #[test]
-    fn leg_lengths() {
-    let my_presses = DIR_KEYPAD
-        .iter()
-        .filter(|&k| *k != 'x')
-        .flat_map(|&ki| {
-            DIR_KEYPAD
-                .iter()
-                .filter(|&k| *k != 'x')
-                .map(move |&kf| ((ki, kf), 1))
-        })
-        .collect();
-    let rob_presses = calc_next(&my_presses, &DIR_KEYPAD);
-    assert_eq!(rob_presses[&('A', 'A')], 1);
-    assert_eq!(rob_presses[&('<', '>')], 3);
-    assert_eq!(rob_presses[&('<', 'A')], 4);
     }
 }
